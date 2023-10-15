7975db7e0c2e1707a5547862ca5a3698cbdf3e90
MinhajJamraiz <mjamraiz1@gmail.com>
2023-10-15 22:39:06 +0500

    Envirenment vars and Eslint + Prettier configure

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 // ------------------------START SERVER-----------------------------------
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
